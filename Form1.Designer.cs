973c979e89f1b9c3a876a6f7891cfbbaf796641f
Azamat Yamaev <azamat.yamaev@mail.ru>
2022-03-12 17:04:09 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace GenshDmg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.persList = new System.Windows.Forms.ComboBox();
+            this.weapList = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(280, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(280, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(280, 120);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(280, 153);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(280, 188);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(280, 223);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Атака оружия";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(163, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Атака героя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(163, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Доп атака%";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(163, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Доп атака";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(163, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Бонус урона%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(163, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Крит урон%";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(163, 330);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Бонус реакции%";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(163, 298);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Модификатор%";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(163, 263);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(109, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Урон способности%";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(280, 256);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 14;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(280, 291);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(100, 20);
+            this.textBox8.TabIndex = 13;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(280, 327);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 20);
+            this.textBox9.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(152, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 37);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Считать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(280, 398);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(100, 20);
+            this.textBox10.TabIndex = 19;
+            // 
+            // persList
+            // 
+            this.persList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.persList.FormattingEnabled = true;
+            this.persList.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.persList.Location = new System.Drawing.Point(484, 50);
+            this.persList.Name = "persList";
+            this.persList.Size = new System.Drawing.Size(121, 21);
+            this.persList.TabIndex = 20;
+            this.persList.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // weapList
+            // 
+            this.weapList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.weapList.FormattingEnabled = true;
+            this.weapList.Location = new System.Drawing.Point(484, 84);
+            this.weapList.Name = "weapList";
+            this.weapList.Size = new System.Drawing.Size(121, 21);
+            this.weapList.TabIndex = 21;
+            this.weapList.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::GenshDmg.Properties.Resources.Tort;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(720, 601);
+            this.Controls.Add(this.weapList);
+            this.Controls.Add(this.persList);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.ComboBox persList;
+        private System.Windows.Forms.ComboBox weapList;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace GenshDmg
+{
+    public partial class Form1 : Form
+    {
+        private enum weaponType { bow, catalyst, sword, claymore, polearm };
+        private string[] characterList;
+        private string[] weaponsList = new string[200];
+        private string[] currentHeroWeapons = new string[200]; 
+        private void LoadPers()
+        {
+            characterList = File.ReadAllLines("../../res/Pers/pers.txt");
+            foreach (string l in characterList)
+            {
+                persList.Items.Add(Split(3, l));
+            };
+        }
+        public string Split(int index, string l)
+        {
+            return l.Split('|')[index].Trim();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            LoadPers();
+            LoadWeapons();
+            persList.SelectedIndex = 0;
+        }
+        private void LoadWeapons()
+        {
+            weaponsList = File.ReadAllLines("../../res/Weap/weapons.txt");
+            
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double hero_atk = Double.Parse(textBox1.Text);
+            double weap_atk = Double.Parse(textBox2.Text);
+            double bon_atk_perc = Double.Parse(textBox3.Text);
+            double bon_atk = Double.Parse(textBox4.Text);
+            double bon_dmg = Double.Parse(textBox5.Text);
+            double krit_dmg = Double.Parse(textBox6.Text);
+            double skill_dmg = Double.Parse(textBox7.Text);
+            double modif = Double.Parse(textBox8.Text);
+            double bon_ms = Double.Parse(textBox9.Text);
+            double dmg = (((hero_atk + weap_atk) + (100 + bon_atk_perc) / 100) + bon_atk) * ((100 + krit_dmg) / 100) * (skill_dmg / 100) * (modif / 100) * ((100 + bon_ms) / 100);
+            textBox10.Text = dmg.ToString();
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = ""; 
+            textBox4.Text = "";
+            textBox5.Text = ""; 
+            textBox6.Text = "";
+            textBox7.Text = "";
+            textBox9.Text = "";
+            textBox8.Text = "";
+            double weap_atk; string bonus_stat_weapon;
+            string currentWeapon = weaponsList[weapList.SelectedIndex];
+            weap_atk = double.Parse(Split(2, currentWeapon));
+            if (currentWeapon.Split('|').Length > 3)
+            {
+                if (Split(4, currentWeapon).Contains("."))
+                    bonus_stat_weapon = Split(4, currentWeapon).Replace('.',',');
+                else 
+                    bonus_stat_weapon = Split(4, currentWeapon);
+            } 
+            else bonus_stat_weapon = "0";
+            textBox2.Text = weap_atk.ToString();
+            if(weaponsList.Length > 3) 
+            {
+                switch (Split(3, currentWeapon))
+                {
+                    case "energy_recharge": textBox7.Text = bonus_stat_weapon.ToString(); break;
+                    case "ms": textBox8.Text = bonus_stat_weapon.ToString(); break;
+                    case "hp": textBox5.Text = bonus_stat_weapon.ToString(); break;
+                    case "krit_dmg": textBox6.Text = bonus_stat_weapon.ToString();  break;
+                    case "krit_chance": textBox4.Text = bonus_stat_weapon.ToString(); break;
+                    case "defenf": textBox2.Text = bonus_stat_weapon.ToString(); break;
+                    case "bonus_atk_percent": textBox3.Text = bonus_stat_weapon.ToString(); break;
+                    case "physical_bonus": textBox1.Text = bonus_stat_weapon.ToString(); break;
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            weapList.Items.Clear();
+            foreach (string l in weaponsList)
+            {
+                if (Split(0, l) == Split(1, characterList[persList.SelectedIndex]))
+                {
+                    weapList.Items.Add(Split(1, l));
+                    //currentHeroWeapons.
+                }
+            }
+            weapList.SelectedIndex = 0;
+
+            //LoadWeapons((weaponType)Enum.Parse(typeof(weaponType), characterList[persList.SelectedIndex].Split('|')[1].Trim()));
+        }
+    }
+}
